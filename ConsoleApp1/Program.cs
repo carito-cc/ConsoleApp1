12fa1710c8da5d4e2bab71278308ceba475dfd09
Carolina <carolina.caroca.c@gmail.com>
2025-09-07 19:30:36 +0200

    project c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+Console.WriteLine("Hello, World!");
+int x = 2;
+int y = 5;
+int sum = x + y;
+Console.WriteLine(sum);
+
+
+Console.Write("Entrez votre nom: ");
+var Nom = Console.ReadLine(Nom);
+
+
+Console.Write("Entre votre age: ");
