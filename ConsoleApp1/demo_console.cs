96e3ad6c807699d2af84af869f65035ad83bfbbb
carito-cc <carolina.caroca.c@gmail.com>
2025-09-13 12:06:52 +0200

    ex1

--- ConsoleApp1/demo_console.cs
+++ ConsoleApp1/demo_console.cs
@@ -1,10 +1,10 @@
 ﻿using System.Runtime.InteropServices.Marshalling;
 
-Console.WriteLine("Hello, World!");
-int x = 2;
-int y = 5;
-int sum = x + y;
-Console.WriteLine(sum);
+//Console.WriteLine("Hello, World!");
+//int x = 2;
+//int y = 5;
+//int sum = x + y;
+//Console.WriteLine(sum);
 
 
 Console.Write("Entrez votre nom: ");
@@ -12,4 +12,6 @@ string Nom = Console.ReadLine();
 Console.WriteLine("Bonjour " + Nom);
 
 Console.Write("Entre votre age: ");
+string Age = Console.ReadLine();
 Console.WriteLine("Bonjour " + Nom + " vous avez " + Age + " ans");
+
